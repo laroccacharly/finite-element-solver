8aa091afb49c0e246db49b06a9148dbd9530f471
Charly <larocca.charly@gmail.com>
2025-02-25 11:29:22 -0500

    fix warning issues

--- src/arma_config.hpp
+++ src/arma_config.hpp
@@ -1,5 +1,10 @@
+#ifndef ARMA_USE_SUPERLU
 #define ARMA_USE_SUPERLU
+#endif
+
+#ifndef ARMA_SUPERLU_INCLUDE_DIR
 #define ARMA_SUPERLU_INCLUDE_DIR /opt/homebrew/include/
+#endif
 
 // Include the original config
 #include <armadillo> 
